29e245651b2aa50ac626bf592cda7491d1068e9f
Xiaofeng Han <xiaofeng.han@oracle.com>
2020-05-14 17:45:46 -0700

    content scripts

--- src/chrome-extension/dev-formatter/background/menu.js
+++ src/chrome-extension/dev-formatter/background/menu.js
@@ -1,17 +1,15 @@
 function onClickHandler(info, tab) {
-    console.log("item " + info.menuItemId + " was clicked");
-    console.log("info: " + JSON.stringify(info));
-    console.log("tab: " + JSON.stringify(tab));
-    alert(info.selectionText);
-};
+  console.log("item " + info.menuItemId + " was clicked");
+  console.log("info: " + JSON.stringify(info));
+  console.log("tab: " + JSON.stringify(tab));
+}
 
 chrome.contextMenus.onClicked.addListener(onClickHandler);
 
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.contextMenus.create({
-        "id": "TestMenu",
-        "title": "Show Selected",
-        "contexts": ["selection"]
-    });
-}
-);
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.contextMenus.create({
+    id: "TestMenu",
+    title: "Show Selected",
+    contexts: ["selection"],
+  });
+});
