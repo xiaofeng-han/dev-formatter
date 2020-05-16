3391c2ba2aa985c94582e498cca7e199246bba1c
Xiaofeng Han <xiaofeng.han@oracle.com>
2020-05-15 23:16:37 -0700

    send message

--- src/chrome-extension/dev-formatter/background/menu.js
+++ src/chrome-extension/dev-formatter/background/menu.js
@@ -2,14 +2,41 @@ function onClickHandler(info, tab) {
   console.log("item " + info.menuItemId + " was clicked");
   console.log("info: " + JSON.stringify(info));
   console.log("tab: " + JSON.stringify(tab));
+  ensureSendMessage(tab.id, "format_selected", (response) => {
+    console.log("message response", response);
+  });
 }
 
 chrome.contextMenus.onClicked.addListener(onClickHandler);
 
 chrome.runtime.onInstalled.addListener(function () {
   chrome.contextMenus.create({
-    id: "TestMenu",
-    title: "Show Selected",
+    id: "FormatSelected",
+    title: "Format Selected",
     contexts: ["selection"],
   });
 });
+
+// Background
+function ensureSendMessage(tabId, message, callback) {
+  chrome.tabs.sendMessage(tabId, { ping: true }, function (response) {
+    if (response && response.pong) {
+      // Content script ready
+      chrome.tabs.sendMessage(tabId, message, callback);
+    } else {
+      // No listener on the other end
+      chrome.tabs.executeScript(
+        tabId,
+        { file: "foreground/bubble.js" },
+        function () {
+          if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError);
+            throw Error("Unable to inject script into tab " + tabId);
+          }
+          // OK, now it's injected and ready
+          chrome.tabs.sendMessage(tabId, message, callback);
+        }
+      );
+    }
+  });
+}
--- src/chrome-extension/dev-formatter/foreground/bubble.js
+++ src/chrome-extension/dev-formatter/foreground/bubble.js
@@ -1,31 +1,42 @@
 // Add bubble to the top of the page.
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.ping) {
+    sendResponse({ pong: true });
+    return;
+  }
+
+  var selection = window.getSelection().toString();
+  console.log("Current selected", selection);
+  sendResponse("Detected selection " + selection);
+  /* Content script action */
+});
 var bubbleDOM = document.createElement("div");
 bubbleDOM.setAttribute("class", "selection_bubble");
 document.body.appendChild(bubbleDOM);
 
-// Lets listen to mouseup DOM events.
-document.addEventListener(
-  "mouseup",
-  function (e) {
-    var selection = window.getSelection().toString();
-    console.log("mouse up detected");
-    console.log("selection", selection);
-    if (selection.length > 0) {
-      renderBubble(e.clientX, e.clientY, selection);
-    }
-  },
-  false
-);
+// // Lets listen to mouseup DOM events.
+// document.addEventListener(
+//   "mouseup",
+//   function (e) {
+//     var selection = window.getSelection().toString();
+//     console.log("mouse up detected");
+//     console.log("selection", selection);
+//     if (selection.length > 0) {
+//       renderBubble(e.clientX, e.clientY, selection);
+//     }
+//   },
+//   false
+// );
 
-// Close the bubble when we click on the screen.
-document.addEventListener(
-  "mousedown",
-  function (e) {
-    bubbleDOM.style.visibility = "hidden";
-    console.log("mouse down detected");
-  },
-  false
-);
+// // Close the bubble when we click on the screen.
+// document.addEventListener(
+//   "mousedown",
+//   function (e) {
+//     bubbleDOM.style.visibility = "hidden";
+//     console.log("mouse down detected");
+//   },
+//   false
+// );
 
 // Move that bubble to the appropriate location.
 function renderBubble(mouseX, mouseY, selection) {
